e8a1b3058544a994a796aa877400c55949fc3631
RebeccaEvans <evans.rebeccaj@gmail.com>
2019-12-10 19:20:39 -0800

    add check suggestion function and counter

--- js/main.js
+++ js/main.js
@@ -1,35 +1,46 @@
 var typewriter = document.getElementById('typewriter')
 var parent = document.getElementById('clue-game')
+var counter = document.getElementById('counter')
+var suggestionButton = document.getElementById('submit')
 const suspectList = ["mustard", "green", "peacock", "scarlett", "white", "plum"]
 const weaponList = ["dagger", "candlestick", "lead_pipe", "revolver", "rope", "wrench"]
 const roomList = ["kitchen", "ballroom", "conservatory", "dining", "billiards", "library", "lounge", "hall", "study"]
+var startButton = document.getElementById('start-game')
 let suspectIndex = Math.floor(Math.random()*5)
 let roomIndex = Math.floor(Math.random()*8)
 let weaponIndex = Math.floor(Math.random()*5)
 let who = ''
 let where = ''
 let withWhat = ''
+let whoGuess = ''
+let whereGuess = ''
+let withGuess = ''
+let guessArr = []
+let solveArr = []
+
 let triesLeft  = 10
 
+//function for event listener
 function handleClick() {
+	startButton.style.visibility = 'hidden'
+	counter.style.visibility = 'visible'
 	getMurderer()
 }
 
-
-function insertAfter(el, referenceNode) {
-    referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+function checkSuggestion() {
+	triesLeft -= 1
+	if (triesLeft < )
+	counter.innerText = `TRIES LEFT: ${triesLeft}`
+	if (`${who}` === `${whoGuess}` && `${where}` === `${whereGuess}` && `${withWhat}` === `${withGuess}`) {
+		console.log('you win!!')
+	}
+	console.log(`${whoGuess}`,`${whereGuess}`,`${withGuess}`)
+	console.log(`${who}`, `${where}`,`${withWhat}`)
 }
 
-typewriter.addEventListener('animationend', (e) => {
-	e.target.style.display = 'none'
-	var startButton = document.getElementById('start-game')
-	addStartListener(startButton)
-})
-
-function addStartListener(id) {
-	id.addEventListener('click',handleClick, true)
-}
-
+//event listeners
+startButton.addEventListener('click',handleClick, true)
+suggestionButton.addEventListener('click', checkSuggestion)
 
 function getMurderer() {
 	let suspectIndex = Math.floor(Math.random()*5)
@@ -40,11 +51,26 @@ function getMurderer() {
 	withWhat = weaponList[weaponIndex]
 	document.getElementById('start-game').removeEventListener('click', handleClick, true)
 	console.log (`${who} ${where} ${withWhat}`)
-	playGame()
+	makeSuggestions()
 }
 
+function trackSuggestion () {
+	guessArr = [`${whoGuess}`,`${whereGuess}`,`${withGuess}`]
+	console.log(guessArr)
+	let arrCount = 0
+	for (var i = 0; i<3; i++) {
+		if (guessArr[i] != '') {
+			arrCount += 1
+		}
+	
+	if (arrCount === 3) {
+		console.log('ready to check suggestions')
+		suggestionButton.style.visibility = 'visible'
+	}
+	}
+}
 
-function playGame () {
+function makeSuggestions () {
 	let chooseWeapons = document.querySelectorAll('.weapons')
 	let chooseSuspects = document.querySelectorAll('.suspects')
 	let chooseRooms = document.querySelectorAll('.room')
@@ -59,28 +85,32 @@ function playGame () {
 		chooseSuspects[i].addEventListener('click', (e) => {
 			if (e.target.id === 'mustard') {
 				suspectGuess.innerHTML = 'Mr. Mustard killed Mr. Boddy ...'
+				whoGuess = 'mustard'
+
 			}
 			else if (e.target.id ==='peacock') {
 				suspectGuess.innerHTML = 'Mrs. Peacock killed Mr. Boddy ...'
-
+				whoGuess = 'peacock'
 			}
 			else if (e.target.id ==='scarlett') {
 				suspectGuess.innerHTML = 'Miss Scarlett killed Mr. Boddy ...'
-
+				whoGuess = 'scarlett'
 			}
 			else if (e.target.id ==='green') {
 				suspectGuess.innerHTML = 'Mr. Green killed Mr. Boddy ...'
-
+				whoGuess = 'green'
 			}
 			else if (e.target.id ==='white') {
 				suspectGuess.innerHTML = 'Mrs. White killed Mr. Boddy ...'
-
+				whoGuess = 'white'
 			}
 			else if (e.target.id ==='plum') {
 				suspectGuess.innerHTML = 'Professor Plum killed Mr. Boddy ...'
+				whoGuess = 'plum'
 			}
 			suspectBox.style.backgroundColor = 'yellow'
 			suspectBox.style.color = 'black'
+			trackSuggestion()
 		})
 	}
 
@@ -89,41 +119,43 @@ function playGame () {
 		chooseRooms[j].addEventListener('click', (e) => {
 			if (e.target.id === 'kitchen') {
 				roomGuess.innerHTML = 'in the Kitchen'
+				whereGuess = 'kitchen'
 			}
 			else if (e.target.id ==='ballroom') {
 				roomGuess.innerHTML = 'in the Ballroom'
-
+				whereGuess = 'ballroom'
 			}
 			else if (e.target.id ==='conservatory') {
 				roomGuess.innerHTML = 'in the Conservatory'
-
+				whereGuess = 'conservatory'
 			}
 			else if (e.target.id ==='dining') {
 				roomGuess.innerHTML = 'in the Dining Room'
-
+				whereGuess = 'dining'
 			}
 			else if (e.target.id ==='billiards') {
 				roomGuess.innerHTML = 'in the Billiards Room'
-
+				whereGuess = 'billiards'
 			}
 			else if (e.target.id ==='library') {
 				roomGuess.innerHTML = 'in the Library'
-
+				whereGuess = 'library'
 			}
 			else if (e.target.id ==='lounge') {
 				roomGuess.innerHTML = 'in the Lounge'
-
+				whereGuess = 'lounge'
 			}
 			else if (e.target.id ==='hall') {
 				roomGuess.innerHTML = 'in the Hall'
-
+				whereGuess = 'hall'
 			}
 			else if (e.target.id ==='study') {
 				roomGuess.innerHTML = 'in the Study'
-
+				whereGuess = 'study'
 			}
 			roomBox.style.backgroundColor = 'yellow'
 			roomBox.style.color = 'black'
+			trackSuggestion()
 		})
 	}
 
@@ -132,33 +164,33 @@ function playGame () {
 		chooseWeapons[k].addEventListener('click', (e) => {
 			if (e.target.id === 'dagger') {
 				weaponGuess.innerHTML = '... with a dagger'
+				withGuess = 'dagger'
 			}
 			else if (e.target.id ==='candlestick') {
 				weaponGuess.innerHTML = '... with a candlestick'
-
+				withGuess= 'candlestick'
 			}
 			else if (e.target.id ==='lead_pipe') {
 				weaponGuess.innerHTML = '...with a lead pipe'
-
+				withGuess = 'lead_pipe'
 			}
 			else if (e.target.id ==='revolver') {
 				weaponGuess.innerHTML = '...with a revolver'
-
+				withGuess = 'revolver'
 			}
 			else if (e.target.id ==='rope') {
 				weaponGuess.innerHTML = '... with a rope'
-
+				withGuess = 'rope'
 			}
 			else if (e.target.id ==='wrench') {
 				weaponGuess.innerHTML = '...with a wrench'
+				withGuess = 'wrench'
 			}
 			weaponBox.style.backgroundColor = 'yellow'
 			weaponBox.style.color = 'black'
+			trackSuggestion()
 		})
 	}
-
-	if (weaponBox.style.backgroundColor === 'yellow' && roomBox.style.backgroundColor === 'yellow' && suspectBox.style.backgroundColor === 'yellow') {
-		console.log('submit suggestion')
-	}
+	return whoGuess, whereGuess, withGuess
 }
 
