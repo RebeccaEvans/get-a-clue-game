df132018d3f42eb5b7a23116c91eaf4d0c9eba47
RebeccaEvans <evans.rebeccaj@gmail.com>
2019-12-11 01:20:26 -0800

    final revisions

--- js/main.js
+++ js/main.js
@@ -2,10 +2,22 @@ var typewriter = document.getElementById('typewriter')
 var parent = document.getElementById('clue-game')
 var counter = document.getElementById('counter')
 var suggestionButton = document.getElementById('submit')
+var message = document.getElementById('message')
+var guessAgain = document.getElementById('new-guess')
+var chooseWeapons = document.querySelectorAll('.weapons')
+var chooseSuspects = document.querySelectorAll('.suspects')
+var chooseRooms = document.querySelectorAll('.room')
+var suspectGuess = document.getElementById('who')
+var suspectBox = document.getElementById('suspect-guess')
+var roomGuess = document.getElementById('where')
+var roomBox = document.getElementById('room-guess')
+var weaponGuess = document.getElementById('with')
+var weaponBox = document.getElementById('weapon-guess')
+var startButton = document.getElementById('start-game')
+var playAgain = document.getElementById('play-again')
 const suspectList = ["mustard", "green", "peacock", "scarlett", "white", "plum"]
 const weaponList = ["dagger", "candlestick", "lead_pipe", "revolver", "rope", "wrench"]
 const roomList = ["kitchen", "ballroom", "conservatory", "dining", "billiards", "library", "lounge", "hall", "study"]
-var startButton = document.getElementById('start-game')
 let suspectIndex = Math.floor(Math.random()*5)
 let roomIndex = Math.floor(Math.random()*8)
 let weaponIndex = Math.floor(Math.random()*5)
@@ -17,31 +29,131 @@ let whereGuess = ''
 let withGuess = ''
 let guessArr = []
 let solveArr = []
-
 let triesLeft  = 10
+
+guessAgain.style.visibility = 'hidden'
+playAgain.style.visibility = 'hidden'
 
 //function for event listener
 function handleClick() {
 	startButton.style.visibility = 'hidden'
 	counter.style.visibility = 'visible'
+	document.getElementById('murder').style.visibility = 'visible'
+	setTimeout(function(){
+		document.getElementById('murder').style.visibility = 'hidden'}, 5000);
 	getMurderer()
-}
-
-function checkSuggestion() {
-	triesLeft -= 1
-	if (triesLeft < )
-	counter.innerText = `TRIES LEFT: ${triesLeft}`
-	if (`${who}` === `${whoGuess}` && `${where}` === `${whereGuess}` && `${withWhat}` === `${withGuess}`) {
-		console.log('you win!!')
-	}
-	console.log(`${whoGuess}`,`${whereGuess}`,`${withGuess}`)
-	console.log(`${who}`, `${where}`,`${withWhat}`)
 }
 
 //event listeners
 startButton.addEventListener('click',handleClick, true)
-suggestionButton.addEventListener('click', checkSuggestion)
 
+function playNewGame() {
+	guessAgain.style.visibility = 'hidden'
+	playAgain.style.visibility = 'hidden'
+	who = ''
+	where = ''
+	withWhat = ''
+	whoGuess = ''
+	whereGuess = ''
+	withGuess = ''
+	guessArr = []
+	solveArr = []
+	triesLeft = 10
+	counter.innerText = `TRIES LEFT: 10`
+	resetMessage()
+	resetSuggestions()
+	document.getElementById('murder').style.visibility = 'visible'
+	setTimeout(function(){
+		document.getElementById('murder').style.visibility = 'hidden'}, 5000);
+	getMurderer()
+}
+//check player suggestion against solve game conditions and display message
+function checkSuggestion() {
+	suggestionButton.style.visibility = 'hidden'
+	suggestionButton.removeEventListener('click', checkSuggestion)
+	triesLeft -= 1
+	counter.innerText = `TRIES LEFT: ${triesLeft}`
+
+	if (triesLeft === 2) {
+		counter.style.backgroundColor = 'red'
+		giveHint()
+	}
+	if (`${who}` === `${whoGuess}` && `${where}` === `${whereGuess}` && `${withWhat}` === `${withGuess}`) {
+		message.innerText = 'YOU WON!'
+		playAgain.style.visibility = 'visible'
+		playAgain.addEventListener('click', playNewGame)
+	}
+	else if (triesLeft === 0) {
+		message.innerText = 'SORRY, YOU ARE OUT OF TRIES -- PLAY AGAIN.' 
+		playAgain.style.visibility = 'visible'
+		playAgain.addEventListener('click', playNewGame)
+	}
+	else {
+		giveHint()
+	}
+}
+
+//give random hint to player
+function giveHint () {
+	let hintNum = Math.floor(Math.random() * 3)
+	if (hintNum === 0) {
+		if (`${who}` === `${whoGuess}`) {
+			message.innerText = 'CLUE: YOU GUESSED THE CORRECT SUSPECT -- REMEMBER TO KEEP TRACK OF YOUR CLUES!!!' 
+		}
+		else {
+			message.innerText = 'CLUE: YOU DID NOT GUESS THE CORRECT SUSPECT -- REMEMBER TO KEEP TRACK OF YOUR CLUES!!!' 
+		}
+	}
+	if (hintNum === 1) {
+		if (`${where}` === `${whereGuess}`) {
+			message.innerText = 'CLUE: YOU GUESSED THE CORRECT LOCATION -- REMEMBER TO KEEP TRACK OF YOUR CLUES!!!' 
+		}
+		else {
+			message.innerText = 'CLUE: YOU DID NOT GUESS THE CORRECT LOCATION -- REMEMBER TO KEEP TRACK OF YOUR CLUES!!!' 
+		}
+	}
+	if (hintNum === 2) {
+		if (`${withWhat}` === `${withGuess}`) {
+			message.innerText = 'CLUE: YOU GUESSED THE CORRECT WEAPON -- REMEMBER TO KEEP TRACK OF YOUR CLUES!!!' 
+		}
+		else {
+			message.innerText = 'CLUE: YOU DID NOT GUESS THE CORRECT WEAPON -- REMEMBER TO KEEP TRACK OF YOUR CLUES!!!' 
+		}
+	}
+	message.style.color = '#fcba03'
+	guessAgain.style.visibility = 'visible'
+	guessAgain.addEventListener('click', newGuess)
+}
+
+function newGuess ()  {
+	guessAgain.style.visibility = 'hidden'
+	message.innerText = 'CLICK ON THE PICTURES BELOW TO FILL THE GUESS BOXES.'
+	whoGuess = ''
+	whereGuess = ''
+	withGuess = ''
+	guessArr = []
+	resetSuggestions()
+}
+
+function resetSuggestions () {
+	suspectGuess.innerText = 'suspect guess'
+	suspectBox.style.backgroundColor = 'black'
+	suspectBox.style.color = 'white'
+	roomGuess.innerText = 'room guess'
+	roomBox.style.backgroundColor = 'black'
+	roomBox.style.color = 'white'
+	weaponGuess.innerText = 'weapon guess'
+	weaponBox.style.backgroundColor = 'black'
+	weaponBox.style.color = 'white'
+	suggestionButton.style.visibility = 'hidden'
+	suggestionButton.removeEventListener('click', checkSuggestion)
+}
+function resetMessage () {
+	message.style.color = 'white'
+	message.innerText = `You have 10 chances to solve the mystery of who killed Mr. Body. Keep track of the clues and solve the mystery before time runs out!`
+}
+
+//game chooses murderer, location and weapon randomly
 function getMurderer() {
 	let suspectIndex = Math.floor(Math.random()*5)
 	let roomIndex = Math.floor(Math.random()*8)
@@ -54,6 +166,7 @@ function getMurderer() {
 	makeSuggestions()
 }
 
+//track player suggestions and update counter
 function trackSuggestion () {
 	guessArr = [`${whoGuess}`,`${whereGuess}`,`${withGuess}`]
 	console.log(guessArr)
@@ -66,20 +179,13 @@ function trackSuggestion () {
 	if (arrCount === 3) {
 		console.log('ready to check suggestions')
 		suggestionButton.style.visibility = 'visible'
+		suggestionButton.addEventListener('click', checkSuggestion)
 	}
 	}
 }
 
+//listen for player suggestions and submission
 function makeSuggestions () {
-	let chooseWeapons = document.querySelectorAll('.weapons')
-	let chooseSuspects = document.querySelectorAll('.suspects')
-	let chooseRooms = document.querySelectorAll('.room')
-	let suspectGuess = document.getElementById('who')
-	let suspectBox = document.getElementById('suspect-guess')
-	let roomGuess = document.getElementById('where')
-	let roomBox = document.getElementById('room-guess')
-	let weaponGuess = document.getElementById('with')
-	let weaponBox = document.getElementById('weapon-guess')
 
 	for (i=0; i<chooseSuspects.length; i++) {
 		chooseSuspects[i].addEventListener('click', (e) => {
@@ -111,6 +217,7 @@ function makeSuggestions () {
 			suspectBox.style.backgroundColor = 'yellow'
 			suspectBox.style.color = 'black'
 			trackSuggestion()
+			resetMessage()
 		})
 	}
 
@@ -156,6 +263,7 @@ function makeSuggestions () {
 			roomBox.style.backgroundColor = 'yellow'
 			roomBox.style.color = 'black'
 			trackSuggestion()
+			resetMessage()
 		})
 	}
 
@@ -189,6 +297,8 @@ function makeSuggestions () {
 			weaponBox.style.backgroundColor = 'yellow'
 			weaponBox.style.color = 'black'
 			trackSuggestion()
+			resetMessage()
+			
 		})
 	}
 	return whoGuess, whereGuess, withGuess
